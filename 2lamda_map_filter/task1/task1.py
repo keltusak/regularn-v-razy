0613cb04cfa31d5b84b0d1e0621895b8a192992c
Jan Balošák <bal41078@spseol.cz>
2024-04-03 14:05:54 +0200

    wow funguje vše kromě 4

--- 2lamda_map_filter/task1/task1.py
+++ 2lamda_map_filter/task1/task1.py
@@ -1,2 +1,9 @@
-def transform_words(words):
-    pass
+def transform_words(words:list) -> list:
+    def transformation(word:str) -> str:
+        return word.upper() + str(len(word))
+
+    transformation = lambda w: w.upper() + str(len(w))
+
+    words = list(map(transformation, words))
+    return words
+   
--- 2lamda_map_filter/task3/task3.py
+++ 2lamda_map_filter/task3/task3.py
@@ -1,2 +1,7 @@
-def add_initials(people):
-    pass
+def add_initials(people:list[dict]) -> list[dict]:
+    def add_initials(p):
+        p["initials"] = p["name"][0] + p["surname"][0]
+        return p
+    
+    return list(map(add_initials, people))
+    
