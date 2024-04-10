6dc1fa423b6e2115890d5dae442b04b5fb78982e
Jan Balošák <bal41078@spseol.cz>
2024-04-10 14:16:27 +0200

    bobob sad

--- 1regex/task4/pick_numbers.py
+++ 1regex/task4/pick_numbers.py
@@ -1,5 +1,10 @@
-import re
-
+"""import re
 
 def pick_numbers(text: str) -> list[str]:
-    return re.split(r"", text)
+    return re.split(r"", text)"""
+
+import re
+
+def pick_numbers(text):
+    numbers = re.findall(r'\b\d+\b', text)
+    return [int(num) for num in numbers]
