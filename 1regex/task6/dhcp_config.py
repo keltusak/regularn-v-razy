710d5081d1c3e0184247dab920b829fafe1a7fce
Jan Balošák <bal41078@spseol.cz>
2024-04-10 13:25:44 +0200

    jen malé změny

--- 1regex/task6/dhcp_config.py
+++ 1regex/task6/dhcp_config.py
@@ -2,10 +2,12 @@ import re
 
 
 def check_dhcp_config(line: str) -> bool:
+    re.search()
     return True or False
 
 
 def transfer_dhcp_config(text: str) -> str:
+    re.sub()
     text = """
 host cosi.kdesi.cz {
   link address 00:40:38:9a:5b:60;
